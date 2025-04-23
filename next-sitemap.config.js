82c73dc540bbed6630e49d37f90a3d467d95090d
midhunpallampetty <midhunpallampetty@gmai.com>
2025-04-23 22:33:03 +0530

    updated sort

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,8 +1,23 @@
 module.exports = {
-    siteUrl: "https://www.eduvia.space", // ✅ no trailing slash!
-    generateRobotsTxt: true,
-    changefreq: "weekly", // optional: how often content is expected to change
-    priority: 0.7,        // optional: importance (0 to 1)
-    sitemapSize: 5000     // optional: split if you have >5000 URLs
-  };
-  
+  siteUrl: "https://www.eduvia.space",
+  generateRobotsTxt: true,
+  changefreq: "daily",
+  priority: 0.7,
+  sitemapSize: 5000,
+  additionalPaths: async (config) => {
+    return [
+      {
+        loc: "https://www.eduvia.space/javascript",
+        changefreq: "daily",
+        priority: 0.8,
+      },
+      {
+        loc: "https://www.eduvia.space/javascript-compiler",
+        changefreq: "daily",
+        priority: 0.8,
+      },
+      
+      // add more as needed
+    ];
+  },
+};
