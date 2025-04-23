7a8c4703123db6596c659759ba58dafa04b743cc
midhunpallampetty <midhunpallampetty@gmai.com>
2025-04-23 09:18:26 +0530

    updated sort

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,17 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { SpeedInsights } from "@vercel/speed-insights/next";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return(
-<>
- <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <meta name="msvalidate.01" content="0604F6F0CDB312F1136B972E1E631B46" />
+      </Head>
 
-<SpeedInsights />
-</>
-  )
+      <Component {...pageProps} />
+      <SpeedInsights />
+    </>
+  );
 }
