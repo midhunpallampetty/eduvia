3ed89edf863341ad1846d2b4e114f2c69643180a
midhunpallampetty <midhunpallampetty@gmai.com>
2025-04-25 23:54:02 +0530

    updated with dynamic page loads

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,10 +3,8 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head>
-        <meta name="yandex-verification" content="00987a11502995ad" />
-        <link rel="icon" href="/icon.png" sizes="50x50" />
-      </Head>
+              <link rel="icon" href="/icon.png" sizes="50x50"/>
+      <Head />
       <body className="antialiased">
         <Main />
         <NextScript />
