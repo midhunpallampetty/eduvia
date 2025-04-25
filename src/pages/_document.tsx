ab33c44f0c886431d46ba7c9360d0400f56f2185
midhunpallampetty <midhunpallampetty@gmai.com>
2025-04-25 23:46:33 +0530

    updated with dynamic page loads

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,7 +4,10 @@ export default function Document() {
   return (
     <Html lang="en">
               <link rel="icon" href="/icon.png" sizes="50x50"/>
-      <Head />
+      <Head>
+      <meta name="yandex-verification" content="00987a11502995ad" />
+
+      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
