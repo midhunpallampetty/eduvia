1c84f79e629c380e9e5e0ca06dd2e57831e4064b
midhunpallampetty <midhunpallampetty@gmai.com>
2025-04-26 11:12:21 +0530

    updated with dynamic page loads

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,44 @@
-import { Html, Head, Main, NextScript } from "next/document";
+// pages/_document.tsx or _document.js
+import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
     <Html lang="en">
-              <link rel="icon" href="/icon.png" sizes="50x50"/>
-      <Head />
-      <body className="antialiased">
+      <Head>
+        {/* Yandex Metrica */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(m,e,t,r,i,k,a){
+                  m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+                  m[i].l=1*new Date();
+                  k=e.createElement(t),a=e.getElementsByTagName(t)[0],
+                  k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+              })(window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+              ym(12345678, "init", {
+                  clickmap:true,
+                  trackLinks:true,
+                  accurateTrackBounce:true,
+                  webvisor:true
+              });
+            `,
+          }}
+        />
+      </Head>
+      <body>
         <Main />
         <NextScript />
+        {/* Optional noscript for Yandex */}
+        <noscript>
+          <div>
+            <img
+              src="https://mc.yandex.ru/watch/101408515"
+              style={{ position: "absolute", left: "-9999px" }}
+              alt=""
+            />
+          </div>
+        </noscript>
       </body>
     </Html>
   );
